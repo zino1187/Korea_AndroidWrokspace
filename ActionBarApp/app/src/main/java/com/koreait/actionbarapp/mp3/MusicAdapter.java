a0379e3dbf534e1cd15dc10ce61d7a2785934885
DESKTOP-GMR5CIL\zino1 <zino1187@naver.com>
2021-01-28 11:16:35 +0900

    c

--- ActionBarApp/app/src/main/java/com/koreait/actionbarapp/mp3/MusicAdapter.java
+++ ActionBarApp/app/src/main/java/com/koreait/actionbarapp/mp3/MusicAdapter.java
@@ -1,32 +1,47 @@
 package com.koreait.actionbarapp.mp3;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.koreait.actionbarapp.MainActivity;
 import com.koreait.actionbarapp.R;
 
 /*ListView에게 데이터 정보를 제공할 어댑터 정의*/
 public class MusicAdapter extends BaseAdapter {
     int[] files= {R.raw.music1, R.raw.music2, R.raw.music3};
+    LayoutInflater layoutInflater;
+    MainActivity mainActivity;
+
+    public MusicAdapter(MainActivity mainActivity){
+        this.mainActivity = mainActivity;
+        layoutInflater = mainActivity.getLayoutInflater();
+    }
 
     //몇건?
     public int getCount() {
-        return 0;
+        return files.length;
     }
 
     //지정한 위치의 데이터 반환
     public Object getItem(int position) {
-        return null;
+        return files[position];
     }
 
     //지정한 위치의 아이디 반환(식별값:개발자가 결정)
     public long getItemId(int position) {
-        return 0;
+        return 0; //pk
     }
 
     //지정한 위치에 들어갈 뷰 반환
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View view=null;
+        if(convertView==null){//최초로 등장하는 아이템
+            view = layoutInflater.inflate(R.layout.item_music, parent, false);
+        }else{
+            view = convertView;
+        }
+        return view;
     }
 }
--- ActionBarApp/app/src/main/java/com/koreait/actionbarapp/mp3/MusicFragment.java
+++ ActionBarApp/app/src/main/java/com/koreait/actionbarapp/mp3/MusicFragment.java
@@ -4,19 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.koreait.actionbarapp.MainActivity;
 import com.koreait.actionbarapp.R;
 
 public class MusicFragment extends Fragment {
+    ListView listView;
+    MusicAdapter musicAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //프레그먼트가 사용할 디자인 인플레이션!!!
         View view = inflater.inflate(R.layout.fragment_music, container, false);
+        listView = (ListView)view.findViewById(R.id.listView);
+        musicAdapter = new MusicAdapter((MainActivity) this.getContext());
+        listView.setAdapter(musicAdapter); //리스트뷰와  어댑터 연결
+
         return view;//인플레이션 결과 반환!!!(우리의 경우 ConstraintLayout 임)
     }
 }
