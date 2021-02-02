e5bcdd2befdb1c5e930b2f395c72e2fa8e4d7e8a
DESKTOP-GMR5CIL\zino1 <zino1187@naver.com>
2021-02-02 11:51:55 +0900

    c

--- GraphicApp/app/src/main/java/com/koreait/graphicapp/gallery/GalleryActivity.java
+++ GraphicApp/app/src/main/java/com/koreait/graphicapp/gallery/GalleryActivity.java
@@ -1,14 +1,53 @@
-package com.koreait.graphicapp;
+package com.koreait.graphicapp.gallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.koreait.graphicapp.R;
 
 public class GalleryActivity extends AppCompatActivity {
+    GalleryView galleryView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        galleryView = findViewById(R.id.galleryView);
+    }
+    public void prev(View view){}
+    public void stop(View view){}
+
+    public void auto(View view){
+        Thread thread = new Thread(){
+            public void run() {
+                for(int i=0;i<galleryView.imgUrl.length-1;i++) {
+                    //배열보다 작을때까지만.. 예)  9까지 존재할경우 8까지는 허용
+                    galleryView.index++;
+                    galleryView.loadImage(galleryView.imgUrl[galleryView.index]);
+                    galleryView.invalidate();//갱신  onDraw() 재호출
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thread.start();
+    }
+
+    public void next(View view){
+        if(galleryView.index< galleryView.imgUrl.length-1) {
+            //배열보다 작을때까지만.. 예)  9까지 존재할경우 8까지는 허용
+            galleryView.index++;
+            galleryView.loadImage(galleryView.imgUrl[galleryView.index]);
+            galleryView.invalidate();//갱신  onDraw() 재호출
+        }else{
+            Toast.makeText(this,"더이상 이미지가 없습니다",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- GraphicApp/app/src/main/java/com/koreait/graphicapp/gallery/GalleryView.java
+++ GraphicApp/app/src/main/java/com/koreait/graphicapp/gallery/GalleryView.java
@@ -1,4 +1,67 @@
 package com.koreait.graphicapp.gallery;
 
-public class GalleryView {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+//URL이미지를 보여줄 뷰
+public class GalleryView extends View {
+    int index; //배열 접근용 인덱스 0,1,2...
+
+    String[] imgUrl={
+            "http://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg"
+            ,"https://image-notepet.akamaized.net/resize/620x-/seimage/20190222%2F88df4645d7d2a4d2ed42628d30cd83d0.jpg"
+            ,"http://image.dongascience.com/Photo/2017/03/14900752352661.jpg"
+            ,"https://cdn.imweb.me/upload/S201807025b39d1981b0b0/5cac274d00b12.jpg"
+            ,"http://www.petzzi.com/data/editor/1601/405ec2eaaa3b9787ab2dcbdf7e7edf5e_1453873939_0913.jpg"
+            ,"https://image.ytn.co.kr/general/jpg/2019/0514/201905141450310305_d.jpg"
+            ,"https://i.pinimg.com/originals/6c/63/86/6c638603fb2bfa90bf0f54641491b855.jpg"
+            ,"https://i.pinimg.com/564x/fb/ae/df/fbaedf3e5349215bd7576049b54b42ff.jpg"
+            ,"https://i.pinimg.com/originals/d0/35/d2/d035d2f3ed9da59e3f0569242d8cf401.jpg"
+            ,"https://i.pinimg.com/originals/8c/47/65/8c4765ac7a0e7029c3ede3178198e81e.jpg"
+    };
+    Bitmap bitmap;
+
+    public GalleryView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        loadImage(imgUrl[index]);
+  }
+
+    public void loadImage(String path){
+        //네트워크 작업이므로, 쓰레드 필요
+        Thread thread = new Thread(){
+            public void run() {
+                try {
+                    URL url = new URL(path);
+                    bitmap = BitmapFactory.decodeStream(url.openStream());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, 0, 0, null);
+    }
 }
+
+
+
+
+
+
