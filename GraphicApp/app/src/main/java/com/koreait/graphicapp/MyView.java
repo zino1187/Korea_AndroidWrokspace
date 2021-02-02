ec4595dc277ace8aac5c038fcb8085a726e602d5
DESKTOP-GMR5CIL\zino1 <zino1187@naver.com>
2021-02-02 10:55:55 +0900

    c

--- GraphicApp/app/src/main/java/com/koreait/graphicapp/MyView.java
+++ GraphicApp/app/src/main/java/com/koreait/graphicapp/MyView.java
@@ -15,6 +15,8 @@ public class MyView extends View {
     String TAG=this.getClass().getName();
     Paint paint ;
     int x=300,y=300;
+    Thread gameThread;//메인쓰레드를 무한루프에 빠지게 해서는 안되므로, 별도의 쓰레드사용
+    boolean gameFlag=true; //쓰레드를 실행할지 여부를 결정
 
     //자바에서 인스턴스 생성할 거면, 아래의 생성자만 두면 되고
     public MyView(Context context) {
@@ -29,7 +31,6 @@ public class MyView extends View {
         Log.d(TAG, "MyView(Context context, AttributeSet attrs) 생성자 호출");
         Log.d(TAG, "attrs is : "+attrs);
         paint = new Paint(Color.RED);
-
     }
     //자바스탠다드처럼, 모든 컴포넌트는 스스로를 그린다..
     //따라서 개발자가 그래픽처리를 주도하려면, onDraw() 메서드를 재정의하면 된다!!
@@ -40,6 +41,26 @@ public class MyView extends View {
         //사각형 객체 생성
         Rect rect=new Rect(x,y,x+100,y+100);
         canvas.drawRect(rect, paint);
+    }
 
+    //무한루프
+    public void gameLoop(){
+        while(gameFlag) {
+            x++;
+            y++;
+            this.invalidate();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
+
+
+
+
+
+
