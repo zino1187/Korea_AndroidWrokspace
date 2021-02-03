2201fd1f5e4323408b394a4e2c8ac1b470859ce2
DESKTOP-GMR5CIL\zino1 <zino1187@naver.com>
2021-02-03 14:28:21 +0900

    c

--- WebSocketClient/app/src/main/java/com/koreait/websocketclient/BoardAdapter.java
+++ WebSocketClient/app/src/main/java/com/koreait/websocketclient/BoardAdapter.java
@@ -4,6 +4,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,6 +43,19 @@ public class BoardAdapter extends BaseAdapter {
         }else{//아니라면, 새롭게 인플레이션..
             view = layoutInflater.inflate(R.layout.item_board, parent, false);//인플레이션시킬거임..(새롭게 생성할 것임)
         }
+        //뷰에 데이터 채우기!!
+        TextView t_title = view.findViewById(R.id.t_title);
+        TextView t_writer = view.findViewById(R.id.t_writer);
+        TextView t_regdate = view.findViewById(R.id.t_regdate);
+        TextView t_hit = view.findViewById(R.id.t_hit);
+
+        Board board = boardList.get(position);
+
+        t_title.setText(board.getTitle());
+        t_writer.setText(board.getWriter());
+        t_regdate.setText(board.getRegdate());
+        t_hit.setText(Integer.toString(board.getHit()));
+
         return view;
     }
 }
